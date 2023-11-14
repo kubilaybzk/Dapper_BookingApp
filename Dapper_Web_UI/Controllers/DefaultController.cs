8dcea02497ba7e0e67c2495587011963775396eb
kubilaybozak <kubilaybozak@gmail.com>
2023-11-14 17:05:57 +0300

    Gereksiz Dosyalar Silinde ve Partial olarak Header ve Footer Ayrıldı.

---
+++ Dapper_Web_UI/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Dapper_Web_UI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
